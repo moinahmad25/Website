f1e558c5e08195871fefda3f9c9317a522d97395
moinahmad25 <110184422+moinahmad25@users.noreply.github.com>
2023-06-26 23:57:50 +0530

    First Commit

---
+++ Contact Us/app.js
@@ -0,0 +1,21 @@
+let homeBtn = document.querySelector(".home");
+let contactBtn = document.querySelector(".contact");
+let informationBtn = document.querySelector(".information");
+let guideBtn = document.querySelector(".guide");
+
+
+homeBtn.addEventListener("click", ()=>{
+    window.location.href = "../index.html";
+})
+contactBtn.addEventListener("click", ()=>{
+    console.log("contact")
+    window.location.href = "index.html";
+})
+informationBtn.addEventListener("click", ()=>{
+    window.location.href = "../Information/index.html";
+    console.log("information")
+})
+guideBtn.addEventListener("click", ()=>{
+    window.location.href = "../Guide/index.html";
+    console.log("guide")
+})
